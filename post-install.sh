e989933bb26efb275e664d069acfe782dc099609
KeithButler-WIT <20089137@mail.wit.ie>
2021-03-03 15:58:15 +0000

    renamed post-install to larbs

--- post-install.sh
+++ post-install.sh
@@ -15,25 +15,12 @@
 #     ln -s ~/workspace/ssh ~/.ssh
 # fi
 
-# wallpaper setup
-# cd
-# mkdir Pictures
-# cd Pictures
-# wget https://github.com/abrochard/spartan-arch/blob/master/wallpaper.jpg?raw=true -O wallpaper.jpg
-# cd ~/.config/
-# mkdir nitrogen
-# cd nitrogen
-# echo '[xin_-1]' > bg-saved.cfg
-# echo "file=/home/$(whoami)/Pictures/wallpaper.jpg" >> bg-saved.cfg
-# echo 'mode=0' >> bg-saved.cfg
-# echo 'bgcolor=#000000' >> bg-saved.cfg
-
 # Enable network daemon
 ln -s /etc/runit/sv/NetworkManager /run/runit/service
 sv up NetworkManager
 
 # using larbs as post install
-wget https://raw.githubusercontent.com/KeithButler-WIT/LARBS/master/larbs.sh -O post-install.sh
-chown $user:$user /home/$user/post-install.sh
-
-sh /home/$user/post-install.sh
+# wget https://raw.githubusercontent.com/KeithButler-WIT/LARBS/master/larbs.sh -O post-install.sh
+curl -LO https://raw.githubusercontent.com/KeithButler-WIT/LARBS/master/larbs.sh
+chown $user:$user /home/$user/larbs.sh
+sh /home/$user/larbs.sh
